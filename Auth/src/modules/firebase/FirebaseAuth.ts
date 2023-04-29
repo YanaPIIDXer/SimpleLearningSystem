4e8f18895e455e540e139f421100176073c500ac
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-29 15:06:29 +0900

    認証・トークン確認メソッド実装

--- Auth/src/modules/firebase/FirebaseAuth.ts
+++ Auth/src/modules/firebase/FirebaseAuth.ts
@@ -21,4 +21,50 @@ export class FirebaseAuth {
   constructor() {
     this.auth = Firebase.auth();
   }
+
+  /**
+   * 新規登録
+   * @param email メールアドレス
+   * @param password パスワード
+   * @param name 名前
+   * @param role ロール
+   */
+  async register(
+    email: string,
+    password: string,
+    name: string,
+    role: string
+  ): Promise<string> {
+    try {
+      const user = await this.auth.createUser({
+        email: email,
+        password: password,
+        displayName: name,
+      });
+      const uid = user.uid;
+      await this.auth.setCustomUserClaims(uid, { role: role });
+      return this.auth.createCustomToken(uid);
+    } catch (error) {
+      console.error("Register Error", error);
+      throw error;
+    }
+  }
+
+  /**
+   * トークン確認
+   * @param idToken トークン（フロントエンド側で取得）
+   * @param role ロール
+   * @returns 問題なければtrue
+   */
+  async verifyToken(idToken: string, role: string): Promise<boolean> {
+    try {
+      const result = await this.auth.verifyIdToken(idToken);
+      const user = await this.auth.getUser(result.uid);
+      const userRole = user.customClaims?.role ?? undefined;
+      return userRole === role;
+    } catch (error) {
+      console.error("Verify Token Error", error);
+      return false;
+    }
+  }
 }
