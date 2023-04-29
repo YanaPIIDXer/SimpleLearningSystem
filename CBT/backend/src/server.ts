4ae8f278e32090e8596bb59c76b37ca389e23a28
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-29 09:25:24 +0900

    CBTのフロントエンド・バックエンドのプロジェクト用意

---
+++ CBT/backend/src/server.ts
@@ -0,0 +1,10 @@
+import Express from "express";
+import CORS from "cors";
+
+const app = Express();
+app.use(Express.json());
+app.use(CORS());
+
+app.listen(3000, () => {
+  console.info("Start CBT Server!");
+});
